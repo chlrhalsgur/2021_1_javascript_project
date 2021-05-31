9301ec78b216f3a37fe46b72bd560de946a55439
chlrhalsgur <chlrhalsgur0209@gmail.com>
2021-05-31 23:56:12 +0900

    day 1 api

--- apiTest1.js
+++ apiTest1.js
@@ -1,17 +1,17 @@
-const obj = {
-    name: 'choi',
-    age: 26,
-    phone: '010-4597-2044'
-}
+var xhr = new XMLHttpRequest();
+var url = 'http://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/getCountrySafetyNewsListNew'; /*URL*/
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+'서비스키'; /*Service Key*/
+queryParams += '&' + encodeURIComponent('serviceKey') + '=' + encodeURIComponent('-'); /**/
+queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('JSON'); /**/
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+queryParams += '&' + encodeURIComponent('cond[country_nm::EQ]') + '=' + encodeURIComponent('가나'); /**/
+queryParams += '&' + encodeURIComponent('cond[country_iso_alp2::EQ]') + '=' + encodeURIComponent('GH'); /**/
+xhr.open('GET', url + queryParams);
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+        alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
+    }
+};
 
-
-
-// object to json
-
-// json = JSON.stringify(obj)
-// console.log(json)
-
-
-// json to object
-obj = JSON.parse(nameJson)
-console.log(obj)
+xhr.send('');
