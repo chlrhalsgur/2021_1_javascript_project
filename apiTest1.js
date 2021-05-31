e3c7ee91c0ab41d046713e4a7712d0f3af784a0f
chlrhalsgur <chlrhalsgur0209@gmail.com>
2021-05-31 14:52:57 +0900

    new test

---
+++ apiTest1.js
@@ -0,0 +1,17 @@
+const obj = {
+    name: 'choi',
+    age: 26,
+    phone: '010-4597-2044'
+}
+
+
+
+// object to json
+
+// json = JSON.stringify(obj)
+// console.log(json)
+
+
+// json to object
+obj = JSON.parse(nameJson)
+console.log(obj)
